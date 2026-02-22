716bb644ac8c0438d414d266841e0ec31cc8dd40
Kamila Díaz <kxdp0001@ce.pucmm.edu.do>
2026-02-22 16:15:46 -0400

    Funcion evaluar, Main

--- Proyecto1_KamilaDiaz_JoseHidalgo_EsmeryVasquez.hs
+++ Proyecto1_KamilaDiaz_JoseHidalgo_EsmeryVasquez.hs
@@ -1,15 +1,18 @@
 {- Proyecto 1 Programación Funcional: Evaluador de Operaciones -}
 
--- TIPOS ALGEBRAICOS
+{- ============TIPOS ALGEBRAICOS============ -}
 -- tipo 'Operador', para separar la estructura de operación
-data Operador = Suma | Resta | Mult | Division
+data Operador = Suma | Resta | Mult | Division 
+
 -- tipo 'Expresion' que representa el árbol de una expresión aritmética, tipo recursivo
-data Expresion = Numero Int | Aplicar Operador Expresion Expresion
+data Expresion = Numero Int | Aplicar Operador Expresion Expresion 
 
--- Aplicar la operación
--- Aplica un operador a dos enteros
--- Devuelve Maybe para manejar errores, Just resultado si la operación es válida y Nothing si ocurre un error
-
+{- ================FUNCIONES================ -}
+{-
+Aplicar la operación
+Aplica un operador a dos enteros
+Devuelve Maybe para manejar errores, Just resultado si la operación es válida y Nothing si ocurre un error
+-}
 aplicarOperacion :: Operador -> Int -> Int -> Maybe Int
 aplicarOperacion Suma x y = Just (x + y)
 aplicarOperacion Resta x y = Just (x - y)
@@ -17,10 +20,41 @@ aplicarOperacion Mult x y = Just (x * y)
 aplicarOperacion Division _ 0 = Nothing
 aplicarOperacion Division x y = Just (x `div` y)
 
---combina dos valores de tipo Maybe Int
---Si alguno de los argumentos es Nothing, devuelve Nothing. Si ambos son Just aplica la función que recibe
---Si hay error en alguna subexpresión, el error se propaga de manera automática.
+{-
+Combina dos valores de tipo Maybe Int
+Si alguno de los argumentos es Nothing, devuelve Nothing. Si ambos son Just aplica la función que recibe
+Si hay error en alguna subexpresión, el error se propaga de manera automática.
+-}
 combinar :: Maybe Int -> Maybe Int -> (Int -> Int -> Maybe Int) -> Maybe Int
 combinar Nothing _ _ = Nothing
 combinar _ Nothing _ = Nothing
 combinar (Just x) (Just y) f = f x y
+
+{-
+Función 'evaluar' recursiva que procesa una expresion representada como un arbol.
+Si es un Numero, devuelve Just n.
+Si es una Aplicacion de operador, evalua ambas subexpresiones y aplica el operador si ambas son validas.
+Si ocurre un error (por ejemplo division por cero),devuelve Nothing.
+-}
+evaluar :: Expresion -> Maybe Int
+evaluar (Numero n) = Just n
+evaluar (Aplicar op e1 e2) =
+    combinar (evaluar e1)
+            (evaluar e2)
+            (aplicarOperacion op)
+
+{- ====================MAIN==================== -}
+-- Main con ejemplo de expresion valida e invalida
+main :: IO ()
+main = do
+    let expresion = 
+            Aplicar Mult 
+                (Aplicar Suma (Numero 8) (Numero 4))
+                (Aplicar Resta (Numero 3) (Numero 1))
+        expresionError = Aplicar Division (Numero 5) (Numero 0)
+
+    putStrLn "Expresion valida '(8 + 4) * (3 - 1)': "
+    print(evaluar expresion)
+    putStrLn "Expresion invalida '5/0': "
+    print(evaluar expresionError)
+    
